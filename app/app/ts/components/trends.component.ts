9005e1acf00d7aeaa2c4177652da362d3a3c3c09
alexserga <snike1985@gmail.com>
2017-03-04 22:35:45 +0200

    styling

--- app/app/ts/components/trends.component.ts
+++ app/app/ts/components/trends.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit, ElementRef} from '@angular/core';
 import { SynchronizeComponent } from "./synchronize.component";
 import { DataBindingService } from "../services/data.binding.service";
 import {Router} from "@angular/router";
@@ -10,16 +10,35 @@ import {Router} from "@angular/router";
 })
 export class TrendsComponent extends SynchronizeComponent implements OnDestroy, OnInit{
 
+    public className: string;
+
     constructor(
         protected dataBindingService:DataBindingService,
-        private router:Router
+        private router:Router,
+        private elementRef: ElementRef
     ){
 
         super( dataBindingService );
-        
 
     }
-    
+
+    public leave(e):void {
+
+        console.log("leave", e);
+        
+        console.log(this.elementRef.nativeElement);
+
+        this.className = "leave-top"
+
+    }
+
+    public enter(e):void {
+
+        console.log("enter", e);
+
+        this.className = "enter-top"
+    }
+
     public ngOnDestroy():void {
         this.unsubscribeData();
 
