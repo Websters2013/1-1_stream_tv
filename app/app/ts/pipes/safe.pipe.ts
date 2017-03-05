42bd6e720ff245a8c5abfc01d3e2961ac623e478
alexserga <snike1985@gmail.com>
2017-03-05 11:59:05 +0200

    changed video base

--- app/app/ts/pipes/safe.pipe.ts
+++ app/app/ts/pipes/safe.pipe.ts
@@ -4,7 +4,8 @@ import { DomSanitizer} from '@angular/platform-browser';
 @Pipe({ name: 'safe' })
 export class SafePipe implements PipeTransform {
     constructor(private sanitizer: DomSanitizer) {}
-    transform(url:string) {
-        return this.sanitizer.bypassSecurityTrustResourceUrl(url + "?autoplay=1");
+    transform(url:string, timestamp:number) {
+        let startTime = Math.floor( Date.now() / 1000 - timestamp;
+        return this.sanitizer.bypassSecurityTrustResourceUrl(url + "?start=" + startTime + "&amp&autoplay=1");
     }
 }
